7e0dc2bdc90dc135a0f40ce1d1b71476ca5ee562
lamb <lamb52gao@gmail.com>
2012-04-11 23:23:34 +0800

    package.json

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var port = 10080;
+var app = express.createServer();
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+
+app.set('view engine', 'jade');
+app.set('view options', {layout:false});
+
+app.get('/index', function (req, res) {
+    res.render('index', {
+        locals:{ pagetitle:'中文' }
+    });
+    console.log("hahas");
+});
+app.all('/views/*.(html|css|js|jpg|png){1}', function(req, res, next){
+    var realpath = __dirname + url.parse(req.url).pathname;
+    console.log(realpath);
+    if(path.existsSync(realpath)){
+        res.end(fs.readFileSync(realpath));
+    }else{
+        res.end('Cannot find request static file: '+req.url);
+    }
+});
+app.listen(port);
