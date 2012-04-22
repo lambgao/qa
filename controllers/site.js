ec628dcea59a960084bf0bcb08d391e9f7197bad
lamb <lamb52gao@gmail.com>
2012-04-22 23:56:52 +0800

    "mongoose": "2.5.13"

--- controllers/site.js
+++ controllers/site.js
@@ -1,4 +1,4 @@
-var $ = require("mongous").Mongous;
+
 exports.index = function(req,res,next){
 	res.render('index', {
 		locals:{ pagetitle:'中文' }
@@ -6,10 +6,7 @@ exports.index = function(req,res,next){
 	console.log("index");
 };
 exports.testdb = function(req,res,next){
-	$("test.games").find({"game" : "pinball"},function(r){
-			//console.log(r);
-	});
-	
+
 	res.render('testdb', {
 		locals:{ pagetitle:'中文' }
 	});
