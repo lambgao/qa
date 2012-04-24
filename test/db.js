22608151a18713f18b50d5b57e9048beadf5295c
lamb <lamb52gao@gmail.com>
2012-04-25 00:23:41 +0800

    test db

---
+++ test/db.js
@@ -0,0 +1,6 @@
+var mongo = require('mongoskin');
+var db = mongo.db('127.0.0.1:27017/test?auto_reconnect=true&poolSize=5');
+db.bind('games');
+db.games.find().toArray(function(err, books){
+console.log(books);
+});
