b885338721555a68902001a7ddcedb5e5cef05b7
lamb <lamb52gao@gmail.com>
2012-04-30 22:18:05 +0800

    pinyintest

--- test/pinyin.js
+++ test/pinyin.js
@@ -7,3 +7,5 @@ console.log(pinyin('孙悟空', false, ','));
 //Sun,Wu,Kong,Xun,Wu,Kong
 
 console.log(pinyin('孙子'));
+console.log(pinyin('???？abcdef'));
+console.log(pinyin('中文', false, ','));
--- test/shell.js
+++ test/shell.js
@@ -1,5 +1,9 @@
 var corpus={
-	"question":"你好吗？",
-	"answer":"我很好",
-	"dirty":0
+	"question": "你好吗？",
+	"answer": "我很好",
+	"index": ['N', 'H', 'M', '？'],
+	"dirty": 0 
+	"read": 0 //0否 1是  默认0
 }
+//dirty 0 否  1是 默认0
+//read  0 否  1是 默认0
